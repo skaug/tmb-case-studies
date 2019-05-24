0f5b1b4baff3682229531ca63a12de552c68434e
olavbr <olavbr@nr.no>
2019-05-24 11:35:36 +0200

    add One step residual example from Thygesen et al.,2017

---
+++ OneStepAheadResiduals/OSA.cpp
@@ -0,0 +1,26 @@
+// Code from Thygesen et al., 2017 is used.
+#include <TMB.hpp>
+template <class Type>
+Type objective_function<Type>::operator()()
+{
+  DATA_VECTOR(y);                  // Observations
+  DATA_VECTOR_INDICATOR(keep, y);  // For one-step predictions
+  DATA_SCALAR(huge);    //Used to integreat out X_1
+  PARAMETER_VECTOR(x);  //The latent AR1 random variable
+  PARAMETER(mu);        //The drift
+  PARAMETER(logsigma);  //Log sd of epsilon
+  PARAMETER(logs);      //Log sd of psi
+  
+  // Initial condition
+  Type nll = -dnorm(x(0), Type(0), huge, true);
+  
+  // Increments
+  for (int i = 1; i < x.size(); ++i)
+    nll -= dnorm(x(i), x(i - 1) + mu, exp(logsigma), true);
+  
+  // Observations
+  for (int i = 0; i < y.size(); ++i)
+    nll -= keep(i) * dnorm(y(i), x(i), exp(logs), true);
+  
+  return nll;
+}
